7eb993bd9d7309f547f717bcab7c29e0c558ddab
Karimi Eliud Maina <eliudkarimi20@gmail.com>
2026-04-23 23:54:37 +0300

    Update API key retrieval method

--- barbershop.py
+++ barbershop.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 
 # export OPENAI_API_KEY
 
-client = OpenAI(api_key=os.getenv("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"))
+client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
 
 # BARBERSHOP DATA FROM THE POSTER
 
