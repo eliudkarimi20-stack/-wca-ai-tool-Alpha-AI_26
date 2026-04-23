9ec4503d56c0b4d4ff5e8f953134365b0654d02e
okutoi brown <okutoibrown@gmail.com>
2026-04-23 23:22:31 +0300

    Update barbershop.py

--- barbershop.py
+++ barbershop.py
@@ -9,3 +9,12 @@ from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("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"))
 
+# BARBERSHOP DATA FROM THE POSTER
+
+spa_data = {
+    "haircuts": {
+        "kids_bald": 50,
+        "kids_styling": 100,
+        "adult_bald": 150,
+        "adult_styling": 200,
+        "cut_only": 50
